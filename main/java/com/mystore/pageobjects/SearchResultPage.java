407cf15c31e9932aa163187ae34187c4abff87c3
mounikach820 <110972189+mounikach820@users.noreply.github.com>
2022-08-10 15:40:50 +0530

    Add files via upload

---
+++ main/java/com/mystore/pageobjects/SearchResultPage.java
@@ -0,0 +1,33 @@
+package com.mystore.pageobjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.mystore.actiondriver.Action;
+import com.mystore.base.Baseclass;
+
+public class SearchResultPage extends Baseclass 
+{
+	
+	Action action= new Action();
+	
+//	Click on the product
+	@FindBy(xpath="//*[@id=\"center_column\"]//img") private WebElement productResult;
+	
+	public SearchResultPage()
+	{
+		PageFactory.initElements(getDriver(), this);
+	}
+	
+	public boolean isProductAvailable() throws Throwable
+	{
+		return action.isDisplayed(getDriver(), productResult);
+	}
+	
+	public AddToCartPage clickOnProduct() throws Throwable 
+	{
+		action.click(getDriver(), productResult);
+		return new AddToCartPage();
+	}
+	
+}
